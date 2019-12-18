95e483b6f5dc4cc41a31c31d1f0aef1b812c3fea
cmurja <55098908+cmurja@users.noreply.github.com>
2019-12-18 11:44:15 -0500

    oooooofff

--- public/index.js
+++ public/index.js
@@ -1,32 +1,62 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
 
 //define paths for express
 const publicDirectoryPAth= path.join(__dirname, "../public");
+constpartialsPath= path.join(_dirname, "../templates/partials");
 const viewsPath = path.join(__dirname,"../templates/views");
 
 
 
 
 // setup handlebars engine and views location
-app.set("views engine", "hbs");// tell express/node to use handlebars for templates
-app.set("views", viewsPath);//tel express to get templates/views folder
-
-app.get("", (req, res) => {
+app.set("view engine", "hbs");// tell express/node to use handlebars for templates
+app.set("view", viewsPath);//tel express to get templates/views folder
+hbs.registerPartials(partialsPath);
+app.get("", async (req, res) => {
   try {
-    res.render("index");
+    res.render("index", {
+      title:"Our first express page"
+    });
   }catch (error){
     res.status(500).send("page not found");
   }
 });
-app.get("/thamisucks", (req, res) => {
+app.get("/thamisucks", async (req, res) => {
   res.send("Thami sucks. And Yes I have to do this while you eat");
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
   console.log(__dirname);
 });
+
+
+app.get("/about/:id", async (req, res) => {
+  const name =req.params.id
+  try {
+    res.render("index", {
+      title: `${name}`
+    });
+  }catch (error){
+    res.status(500).send("page not found");
+  }
+});
+
+
+app.get("/swag", async (req, res) => {
+ try {
+    res.render("swag");
+      title: name
+    });
+  }catch {
+    res.status(500).send("page not found");
+  }
+});
+
+
+
 
 //127.0.0.1:3000
