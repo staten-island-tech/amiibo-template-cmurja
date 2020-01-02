6558df2319d398f8b21972186ca55c45593dba3d
cmurja <55098908+cmurja@users.noreply.github.com>
2020-01-02 11:44:47 -0500

    fix webpack

--- public/index.js
+++ public/index.js
@@ -3,59 +3,56 @@ const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
-
 //define paths for express
-const publicDirectoryPAth= path.join(__dirname, "../public");
-constpartialsPath= path.join(_dirname, "../templates/partials");
-const viewsPath = path.join(__dirname,"../templates/views");
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-
-
-
-// setup handlebars engine and views location
-app.set("view engine", "hbs");// tell express/node to use handlebars for templates
-app.set("view", viewsPath);//tel express to get templates/views folder
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views and location
+app.set("view engine", "hbs"); //Telling express/node to use handlebars for our templates
+app.set("views", viewsPath);//telling express to get templates from tamples/views folder
 hbs.registerPartials(partialsPath);
+//set up our static asset directory 
+app.use(express.static(publicDirectoryPath));
 app.get("", async (req, res) => {
-  try {
-    res.render("index", {
-      title:"Our first express page"
-    });
-  }catch (error){
-    res.status(500).send("page not found");
-  }
+    try {
+      res.render("index", {
+          title: "Our First Express App"
+      }); 
+    } catch(error) {
+        res.status(500).send();
+    }
 });
-app.get("/thamisucks", async (req, res) => {
-  res.send("Thami sucks. And Yes I have to do this while you eat");
+app.get("/tommysucks", (req, res) => {
+    res.send("Tommy sucks and yes I did this")
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
-  console.log(__dirname);
+  console.log(viewsPath);
 });
-
-
 app.get("/about/:id", async (req, res) => {
-  const name =req.params.id
-  try {
-    res.render("index", {
-      title: `${name}`
-    });
-  }catch (error){
-    res.status(500).send("page not found");
-  }
-});
+    const name = req.params.id;
+    try {
+      res.render("index", {
+          title: `${name}`
+      }); 
+    } catch(error) {
+        res.status(500).send();
+    }
+}); 
+//http://127.0.0.1:3000/
 
 
 app.get("/swag", async (req, res) => {
- try {
-    res.render("swag");
-      title: name
-    });
-  }catch {
-    res.status(500).send("page not found");
-  }
+    const name = req.params.id;
+    try {
+      res.render("swag", {
+          title: `${name}`
+      }); 
+    } catch(error) {
+        res.status(500).send();
+    }
 });
-
 
 
 
